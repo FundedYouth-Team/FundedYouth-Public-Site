fb1b516230966197e3c1d2862d74d062a00e8bed
ryan <drjonesy@users.noreply.github.com>
2026-02-23 19:38:10 -0800

    Removed portal and just kept - Public Site

--- _FirstUserSetup/add_first_admin.sql
+++
@@ -1,5 +0,0 @@
- UPDATE auth.users
-  SET raw_app_meta_data = raw_app_meta_data || '{"role": "admin"}'::jsonb
-  WHERE email = 'your-email@example.com';
-
---   REPLACE with correct email address
--- admin-portal/src/guards/index.ts
+++
@@ -1,3 +0,0 @@
-export { RequireAdmin } from "./RequireAdmin";
-export { RequireAdminOrManager } from "./RequireAdminOrManager";
-export { UnauthorizedRedirect } from "./UnauthorizedRedirect";
